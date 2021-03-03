83d1a5ba8a92f7bbb6dec0bde907f3716b7e5d24
Mr. Coxall <patrick.coxall@ocsb.ca>
2021-03-03 02:33:09 +0000

    lint error

--- basic_math.py
+++ basic_math.py
@@ -6,16 +6,16 @@
 
 
 def main():
-	# this function does basic math
+    # this function does basic math
 
-	print(9 + 2)
-	print("7-3={}".format(7 - 3))
-	print("4/2={}".format(4 / 2))
-	print("4+4/2={}".format(4 + 4 / 2))
-	print("3+4*2={}".format(3 + 4 * 2))
-	print("5+2^3={}".format(5 + 2 ^ 3))  # Bitwise Operators
-	print("5+2³={}".format(5 + 2**3))  # exponent
+    print(9 + 2)
+    print("7-3={}".format(7 - 3))
+    print("4/2={}".format(4 / 2))
+    print("4+4/2={}".format(4 + 4 / 2))
+    print("3+4*2={}".format(3 + 4 * 2))
+    print("5+2^3={}".format(5 + 2 ^ 3))  # Bitwise Operators
+    print("5+2³={}".format(5 + 2**3))  # exponent
 
 
 if __name__ == "__main__":
-	main()
+    main()
